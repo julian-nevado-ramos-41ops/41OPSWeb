249d234b696c9b27d619c7664cc8db087b61e1a3
Julian <julian.nevado.ramos@41ops.com>
2026-02-27 08:29:14 +0100

    new map color

--- src/app/components/interactive-map/interactive-map.component.ts
+++ src/app/components/interactive-map/interactive-map.component.ts
@@ -105,9 +105,9 @@ export class InteractiveMapComponent implements AfterViewInit, OnDestroy {
       scrollWheelZoom: true,
     });
 
-    // Dark CARTO tile layer (matching the reference image)
+    // Voyager CARTO tile layer (light, modern style)
     L.tileLayer(
-      'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png',
       {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
         subdomains: 'abcd',
